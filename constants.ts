a9d2e7c54c3d633b0e4511610eeb4653df2ee035
Yannik Lubas <yannik@Yanniks-MacBook-Pro.local>
2025-01-21 21:14:50 +0100

    (feat): Add stars and forks filter

--- constants.ts
+++ constants.ts
@@ -2,3 +2,5 @@ import * as path from "jsr:@std/path";
 
 export const OUTPUT_BASE = "output";
 export const FILTERED_JSON = path.join(OUTPUT_BASE, "filtered.json");
+export const FINAL_JSON = path.join(OUTPUT_BASE, "final.json");
+export const TABLE_FILE = path.join(OUTPUT_BASE, "table.txt");
--- stars.ts
+++ stars.ts
@@ -1,15 +1,25 @@
 import { loadJson } from "./load.ts";
-import { FILTERED_JSON } from "./constants.ts";
+import { FILTERED_JSON, FINAL_JSON, TABLE_FILE } from "./constants.ts";
+import { writeFilePretty } from "./write.ts";
 
 async function main() {
+	const threshold = 5;
 	const data = await loadJson(FILTERED_JSON);
 	if (!data.success || data.data === undefined) {
 		console.error(`failed to load ${FILTERED_JSON}: ${data.error}`);
 	} else {
 		const arr = data.data;
 		console.log(`Original entries: ${arr.length}`);
-		const filtered = arr.filter((e) => e.stargazers_count >= 1);
+		const filtered = arr.filter(
+			(e) => e.stargazers_count >= threshold || e.forks_count >= threshold, // ||
+			// e.forks >= threshold,
+		);
 		console.log(`Filtered entries: ${filtered.length}`);
+		await writeFilePretty(FINAL_JSON, filtered);
+		await Deno.writeTextFile(
+			TABLE_FILE,
+			filtered.reduce((ack, curr) => `${ack}\n${curr.html_url}`, ""),
+		);
 	}
 }
 
